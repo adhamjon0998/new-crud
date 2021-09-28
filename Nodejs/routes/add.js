f8dff98eaf9fda25935c3b6df6ceb7d057d831bb
adhamjon0998 <88327177+adhamjon0998@users.noreply.github.com>
2021-09-28 10:22:59 +0500

    Add files via upload

---
+++ Nodejs/routes/add.js
@@ -0,0 +1,29 @@
+const { Router } = require('express')
+const Course = require('../models/Course')
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.render('add', {
+        title: 'Добавить курсы',
+        isAdd: true
+    })
+})
+
+router.post('/', async (req, res) => {
+    const course = new Course({
+        title: req.body.title,
+        price: req.body.price,
+        img: req.body.img,
+        userId: req.user
+    })
+
+    try {
+        await course.save()
+        res.redirect('/courses')
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
+module.exports = router
